b8b3e3610fcc3f98ac25cf145e2f07c1d6a8ab18
Sharmitha Ganesan <sharmithaganesan08@gmail.com>
2022-12-04 23:43:48 -0500

    tf with results

--- cpp_pubsub/src/publisher_member_function.cpp
+++ cpp_pubsub/src/publisher_member_function.cpp
@@ -26,12 +26,13 @@
 #include "geometry_msgs/msg/transform_stamped.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "tf2/LinearMath/Quaternion.h"
-#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_ros/transform_broadcaster.h"
 #include "std_msgs/msg/string.hpp"
 #include "string_srv/srv/change.hpp"
 #include <chrono> // NOLINT
 #include <memory> // NOLINT
 #include <string> // NOLINT
+#include <iostream>
 
 using namespace std::placeholders;    // NOLINT
 using namespace std::chrono_literals; // NOLINT
@@ -46,7 +47,7 @@ class MinimalPublisher : public rclcpp::Node {
    * @brief Construct a new Minimal Publisher object
    *
    */
-  explicit MinimalPublisher(char * transformation[]) : Node("minimal_publisher"), count_(0) {
+  MinimalPublisher() : Node("minimal_publisher"), count_(0) {
     if (rcutils_logging_set_logger_level(
             this->get_logger().get_name(),
             RCUTILS_LOG_SEVERITY::RCUTILS_LOG_SEVERITY_DEBUG) ==
@@ -59,15 +60,14 @@ class MinimalPublisher : public rclcpp::Node {
     this->declare_parameter("freq", 100);
     f_num = this->get_parameter("freq").get_parameter_value().get<int>();
     this->set_parameter(rclcpp::Parameter("freq", f_num));
-    tf_static_broadcaster_ = std::make_shared<tf2_ros::StaticTransformBroadcaster>(this);
-
-    // Publish static transforms once at startup
-    this->make_transforms(transformation);
     publisher_ =
         this->create_publisher<std_msgs::msg::String>("talker_bot", 10);
     timer_ = this->create_wall_timer(
         std::chrono::milliseconds(f_num),
         std::bind(&MinimalPublisher::timer_callback, this));
+    // broadcast talk
+    tf_broadcaster_ = std::make_unique<tf2_ros::TransformBroadcaster>(*this);
+    tf_timer_ = this->create_wall_timer(300ms, std::bind(&MinimalPublisher::make_transforms, this));
     // Create a service for modifying str
     std::string string_service_name = "service_topic";
     string_service_ = this->create_service<string_srv::srv::Change>(
@@ -127,60 +127,61 @@ class MinimalPublisher : public rclcpp::Node {
                        "SERVICE CALLED AND BASE STRING CHANGED");
     response->b = "SERVICE DONE";
   }
-  void make_transforms(char * transformation[]) {
+  void make_transforms() {
     geometry_msgs::msg::TransformStamped t;
 
     t.header.stamp = this->get_clock()->now();
     t.header.frame_id = "world";
-    t.child_frame_id = transformation[1];
+    t.child_frame_id = "talk";
 
-    t.transform.translation.x = atof(transformation[2]);
-    t.transform.translation.y = atof(transformation[3]);
-    t.transform.translation.z = atof(transformation[4]);
+    t.transform.translation.x = 5;
+    t.transform.translation.y = 5;
+    t.transform.translation.z = 5;
     tf2::Quaternion q;
-    q.setRPY(
-      atof(transformation[5]),
-      atof(transformation[6]),
-      atof(transformation[7]));
+    q.setRPY(0.7,0.5,1);
     t.transform.rotation.x = q.x();
     t.transform.rotation.y = q.y();
     t.transform.rotation.z = q.z();
     t.transform.rotation.w = q.w();
-
-    tf_static_broadcaster_->sendTransform(t);
+    
+    tf_broadcaster_->sendTransform(t);
   }
-  std::shared_ptr<tf2_ros::StaticTransformBroadcaster> tf_static_broadcaster_;
+  std::unique_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
 
   std_msgs::msg::String message;
   int f_num;
   std::string string_var;
   rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::TimerBase::SharedPtr tf_timer_;
   rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
   rclcpp::Service<string_srv::srv::Change>::SharedPtr string_service_;
   size_t count_;
 };
 
 int main(int argc, char *argv[]) {
-  if (argc != 8) {
-    RCLCPP_INFO(
-      rclcpp::get_logger("rclcpp"), "Invalid number of parameters\nusage: "
-      "$ ros2 run learning_tf2_cpp static_turtle_tf2_broadcaster "
-      "child_frame_name x y z roll pitch yaw");
-    return 1;
-  }
 
-  // As the parent frame of the transform is `world`, it is
-  // necessary to check that the frame name passed is different
-  if (strcmp(argv[1], "world") == 0) {
-    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Your static turtle name cannot be 'world'");
-    return 2;
-  }
+  // if (argc != 8) {
+  //   RCLCPP_INFO(
+  //     rclcpp::get_logger("rclcpp"), "Invalid number of parameters\nusage: "
+  //     "$ ros2 run learning_tf2_cpp static_turtle_tf2_broadcaster "
+  //     "child_frame_name x y z roll pitch yaw");
+  //   return 1;
+  // }
+  
+  // // As the parent frame of the transform is `world`, it is
+  // // necessary to check that the frame name passed is different
+  // if (strcmp(argv[1], "world") == 0) {
+  //   RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Your static turtle name cannot be 'world'");
+  //   return 2;
+  // }
+
+  
   rclcpp::init(argc, argv);
   RCLCPP_INFO_STREAM(rclcpp::get_logger("rclcpp"),
                      "\n MULTIPLES OF 5 set as ERROR\n MULTIPLES OF 5 AND 10 "
                      "set as FATAL \n EVEN NUMBERS set as INFO \n ODD NUMBERS "
                      "set as DEBUG \n SERVICE CALLS enables WARN");
-  rclcpp::spin(std::make_shared<MinimalPublisher>(argv));
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
   rclcpp::shutdown();
   return 0;
 }
---
+++ cpp_pubsub/test/test.cpp
@@ -0,0 +1,2 @@
+#include "rclcpp/rclcpp.hpp"
+#include <gtest/gtest.h>
